a8efa4b0774d21ae44a2dcf971aca47c671f97ae
Nick van der Zwet <nick@connectholland.nl>
2017-03-31 16:52:39 +0200

    adding test function with input

--- src/WebhookBundle/Controller/DefaultController.php
+++ src/WebhookBundle/Controller/DefaultController.php
@@ -2,18 +2,55 @@
 
 namespace WebhookBundle\Controller;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $response = [];
+        $data = $this->getContentAsArray($request);
+        $result = $data->get('result');
+
+        switch ($result['action']) {
+            case 'webhook' :
+                $response = $this->getDistanceBetweenCities($result);
+                break;
+        }
+
+        return new JsonResponse($response);
+    }
+
+    protected function getContentAsArray(Request $request)
+    {
+        $content = $request->getContent();
+
+        if (empty($content)) {
+            throw new BadRequestHttpException('Content is empty');
+        }
+
+        //if(!Validation::isValidJsonString($content)){
+        //    throw new BadRequestHttpException("Content is not a valid json");
+        //}
+
+        return new ArrayCollection(json_decode($content, true));
+    }
+
+    protected function getDistanceBetweenCities($result)
+    {
+        $city1 = $result['parameters']['geo-city1'];
+        $city2 = $result['parameters']['geo-city2'];
+
         $data = [
-            'speech' => 'Test from serene-beach-72762',
-            'displayText' => 'Test from serene-beach-72762',
+            'speech' => 'Test from '.$city1.' to '.$city2,
+            'displayText' => 'Test from'.$city1.' to '.$city2,
             'source' => 'serene-beach-72762',
         ];
-        return new JsonResponse($data);
+
+        return $data;
     }
 }
